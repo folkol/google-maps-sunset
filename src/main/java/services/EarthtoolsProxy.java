19020d2924d4155955053227424fcfe6c6a70af2
Mattias Johansson <mattias.johansson@atex.com>
2014-05-01 21:37:15 +0200

    Some renaming and cleanup

--- src/main/java/services/EarthtoolsProxy.java
+++ src/main/java/services/EarthtoolsProxy.java
@@ -17,15 +17,15 @@ public class EarthtoolsProxy {
 
     @GET
     @Path("{latitude}/{longitude}")
-    public String sunset(@PathParam("latitude") String latitude,
+    public String sun(@PathParam("latitude") String latitude,
                          @PathParam("longitude") String longitude)
     {
-        WebResource resource = Client.create().resource("http://www.earthtools.org/sun");
+        WebResource resource = Client.create().resource("http://www.earthtools.org/sun-1.0");
         Calendar calendar = Calendar.getInstance();
         int day = calendar.get(Calendar.DAY_OF_MONTH);
         int month = 1 + calendar.get(Calendar.MONTH);
 
-        String path = format("%s/%s/%d/%d/%d/%d", latitude, longitude, day, month, 2, 0);
+        String path = format("%s/%s/%d/%d/%d/%d", latitude, longitude, day, month, 1, 0);
         ClientResponse response = resource.path(path).get(ClientResponse.class);
 
         return response.getEntity(String.class);
