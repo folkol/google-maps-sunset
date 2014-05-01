a203ef70538c4804877308ecd11b95a4f915a64a
Mattias Johansson <mattias.johansson@atex.com>
2014-05-01 19:58:11 +0200

    Now using local timezone and date

--- src/main/java/services/EarthtoolsProxy.java
+++ src/main/java/services/EarthtoolsProxy.java
@@ -2,6 +2,8 @@ package services;
 
 import static java.lang.String.*;
 
+import java.util.Calendar;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -10,24 +12,21 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-@Path("")
+@Path("sunset")
 public class EarthtoolsProxy {
 
-    private static final String BASE_URL = "http://www.earthtools.org/sun";
-
     @GET
-    @Path("sunset/{latitude}/{longitude}/{day}/{month}/{timezone}/{dst}")
+    @Path("{latitude}/{longitude}")
     public String sunset(@PathParam("latitude") String latitude,
-                      @PathParam("longitude") String longitude,
-                      @PathParam("day") String day,
-                      @PathParam("month") String month,
-                      @PathParam("timezone") String timezone,
-                      @PathParam("dst") String dst)
+                         @PathParam("longitude") String longitude)
     {
-        Client client = Client.create();
-        String url = format("%s/%s/%s/%s/%s/%s/%s", BASE_URL, latitude, longitude, day, month, timezone, dst);
-        WebResource webResource = client.resource(url);
-        ClientResponse response = webResource.accept("application/xml").get(ClientResponse.class);
+        WebResource resource = Client.create().resource("http://www.earthtools.org/sun");
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        int month = calendar.get(Calendar.DAY_OF_MONTH);
+
+        String path = format("%s/%s/%d/%d/%d/%d", latitude, longitude, day, month, 2, 0);
+        ClientResponse response = resource.path(path).get(ClientResponse.class);
 
         return response.getEntity(String.class);
     }
